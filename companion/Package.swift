b9fc340111c7257f94bd5ef91f630c59fea66352
jiangew <5180293+jiangew@users.noreply.github.com>
2026-08-28 21:17:24 +0800

    fix: require macOS 14 for companion

--- companion/Package.swift
+++ companion/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CodexWatchCompanion",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(name: "codex-watch-companion", targets: ["CodexWatchCompanion"]),
     ],
